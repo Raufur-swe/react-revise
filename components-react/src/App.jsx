c6a2887e657316fd10b8086986069fe910211c20
Raufur <raufurshekh45@gmail.com>
2026-02-07 11:29:53 +0600

    under standing the components

---
+++ components-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/card'
+
+const App = () => {
+  return (
+    <>
+
+<Card/>
+    </>
+  )
+}
+
+export default App
---
+++ components-react/src/Components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+
+<div className="card">
+  <h1>Raufur</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid mollitia molestias beatae neque laborum repellendus sed quis asperiores reprehenderit veritatis, id consequuntur ut dolores quo, saepe consequatur provident in minus?
+  Et exercitationem repudiandae ullam ducimus libero commodi eius ab minima at tempora quibusdam, modi quaerat similique quod doloremque aperiam quam repellat pariatur ea nihil deleniti distinctio corporis cupiditate. Pariatur, odio?</p>
+</div>
+    </div>
+  )
+}
+
+export default Card
