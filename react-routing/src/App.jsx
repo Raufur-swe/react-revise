6037298acae07e59603d5a31741d80fd01f2dd5f
Raufur <raufurshekh45@gmail.com>
2026-02-10 09:27:23 +0600

    BASIC-ROUTING

---
+++ react-routing/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Service from './pages/Service'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/service' element={<Service/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ react-routing/src/pages/Service.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div>Sevice</div>
+  )
+}
+
+export default Service
